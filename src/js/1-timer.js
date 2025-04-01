495de44242b4f6385ebaeddf8d7122d386d976d1
ievgenii_prokopenko <ievgeniiprokopenko@gmail.com>
2025-04-01 18:00:45 +0200

    JS-10

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,8 +1,8 @@
 
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-//import iziToast from "izitoast";
-//import "izitoast/dist/css/iziToast.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
 const datetimePicker = document.querySelector("#datetime-picker");
 const startButton = document.querySelector("[data-start]");
